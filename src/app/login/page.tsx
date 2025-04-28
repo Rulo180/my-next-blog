a1c66326b33685a19cf51479a9bf8ada1ba9a5d3
Martin Valles <mvallesacc@gmail.com>
2025-04-27 22:32:06 -0300

    fix(build): Fix prisma generate issue and type checking issues - Add prisma generate to build process. - Wrap forms with supense.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,24 @@
-'use client';
+"use client";
 
-import { Container } from "@chakra-ui/react";
+import { Container, Flex, Spinner } from "@chakra-ui/react";
+import { Suspense } from "react";
 
 import LoginForm from "@/app/ui/components/LoginForm";
 
 const LoginPage: React.FC = () => {
-    return (
-        <Container centerContent bgColor="gray.100" py="8">
-            <LoginForm />
-        </Container>
-    );
+  return (
+    <Container centerContent bgColor="gray.100" py="8">
+      <Suspense
+        fallback={
+          <Flex justify="center" align="center" height="100%">
+            <Spinner size="xl" color="blue.500" />
+          </Flex>
+        }
+      >
+        <LoginForm />
+      </Suspense>
+    </Container>
+  );
 };
 
 export default LoginPage;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,22 @@
-import SignUpForm from '@/app/ui/components/SignupForm';
-import { Container } from '@chakra-ui/react';
+import { Container, Flex, Spinner } from "@chakra-ui/react";
+import { Suspense } from "react";
+
+import SignUpForm from "@/app/ui/components/SignupForm";
 
 const SignupPage: React.FC = async () => {
-    return (
-        <Container centerContent bgColor="gray.100" py="8">
-            <SignUpForm />
-        </Container>
-    );
+  return (
+    <Container centerContent bgColor="gray.100" py="8">
+      <Suspense
+        fallback={
+          <Flex justify="center" align="center" height="100%">
+            <Spinner size="xl" color="blue.500" />
+          </Flex>
+        }
+      >
+        <SignUpForm />
+      </Suspense>
+    </Container>
+  );
 };
 
 export default SignupPage;
