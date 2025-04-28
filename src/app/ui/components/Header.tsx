193b73b3115d1a1d4430b3bacd00ebbfb15bd2f2
Martin Valles <mvallesacc@gmail.com>
2025-04-28 11:16:43 -0300

    feat(Header): Improve aligment between subtitle and avatar

--- src/app/ui/components/Header.tsx
+++ src/app/ui/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Text, Box } from "@chakra-ui/react";
 
 import { auth } from "@/auth";
 import UserAvatar from "@/app/ui/components/UserAvatar";
@@ -8,10 +8,12 @@ export default async function Header() {
   const session = await auth();
   return (
     <Flex as="header" p={6} direction="column" justifyContent="center" gap={3}>
-      <Flex justifyContent="space-between">
-        <Text as="h3" fontSize="2xl" textAlign="center">
-          A Playground Project for Next.js
-        </Text>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Box flex="1" textAlign="center" pl="20px">
+          <Text as="h3" fontSize="2xl">
+            A Playground Project for Next.js
+          </Text>
+        </Box>
         {session?.user && (
           <UserAvatar name={session.user.name || "User"} />
         )}
