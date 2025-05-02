bdad07698d4950b1266d0b8c7112740b8a2124c3
Martin Valles <mvallesacc@gmail.com>
2025-05-01 22:39:38 -0300

    feat(Footer): Create Footer and migrate components to layout folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import Provider from '@/app/provider';
+import { Grid, GridItem } from "@chakra-ui/react";
 
+import Provider from "@/app/provider";
+import { geistMono, geistSans } from "@/app/ui/fonts";
+import Footer from "@/app/ui/components/layout/Footer";
+import Header from "@/app/ui/components/layout/Header";
+import NavBar from "@/app/ui/components/layout/NavBar";
 import "./globals.css";
-import { geistMono, geistSans } from '@/app/ui/fonts';
-import Header from "@/app/ui/components/Header";
-import NavBar from "@/app/ui/components/NavBar";
-
 
 export const metadata: Metadata = {
   title: "My NextJS Blog",
@@ -23,9 +24,24 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>
-          <Header />
-          <NavBar />
-          {children}
+          <Grid
+            templateRows="auto auto 1fr auto"
+            templateColumns="1fr"
+            height="100vh"
+          >
+            <GridItem>
+              <Header />
+            </GridItem>
+            <GridItem>
+              <NavBar />
+            </GridItem>
+            <GridItem>
+              {children}
+            </GridItem>
+            <GridItem>
+              <Footer />
+            </GridItem>
+          </Grid>
         </Provider>
       </body>
     </html>
---
+++ src/app/ui/components/layout/Footer.tsx
@@ -0,0 +1,16 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+
+import Socials from "@/app/ui/components/Socials";
+
+export default function Footer() {
+  return (
+    <Container as="footer" fontSize="sm" backgroundColor="gray.200">
+      <Flex justifyContent="space-between" alignItems="center" py={4}>
+        <Flex justifyContent="center" gap={4}>
+          <Socials />
+        </Flex>
+        <Text>© 2025 by Martin Valles</Text>
+      </Flex>
+    </Container>
+  );
+}
